e73b21cdfae102916840a255f46c1a0860e1c1d9
Hou Tianze <houtianze@gmail.com>
2014-07-14 22:18:55 +0800

    Add in setup.py for ArchLinux packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+	name='bypy',
+	version='1.0',
+	description='Python client for Baidu Yun (Personal Cloud Storage) 百度云/百度网盘 Python 客户端',
+	author='Hou Tianze',
+	author_email='houtianze@gmail.com',
+	url='https://github.com/houtianze/bypy',
+	packages=['bypy'],
+	)
+
+# vim: set fileencoding=utf-8
