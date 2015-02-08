7ce91f466de790fd2baabf5fd01de496c7fca20f
Hou Tianze <houtianze@gmail.com>
2015-02-08 17:07:52 +0800

    Fix version typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import bypy
 
 setup(
 	name='bypy',
-	version=str(bypy.__version__),
+	version=bypy.__version__,
 	description='Python client for Baidu Yun (Personal Cloud Storage) 百度云/百度网盘 Python 客户端',
 	author='Hou Tianze',
 	url='https://github.com/houtianze/bypy',
