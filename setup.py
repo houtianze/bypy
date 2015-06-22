8f341d8ab8e048248ba153737e0f411cc36ad926
Hou Tianze <houtianze@gmail.com>
2015-06-22 12:25:00 +0800

    Prepare for PyPI

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setup(
 	description='Python client for Baidu Yun (Personal Cloud Storage) 百度云/百度网盘 Python 客户端',
 	author='Hou Tianze',
 	url='https://github.com/houtianze/bypy',
+	download_url='https://github.com/houtianze/bypy/tarball/1.0.20',
 	packages=find_packages(),
-	scripts=["bypy.py","bypygui.pyw"],
+	scripts=['bypy.py', 'bypygui.pyw'],
+	keywords = ['bypy', 'baidu', 'baidu pcs', 'client']
 	)
 
 # vim: set fileencoding=utf-8
