30e3e87a52ea33c5bafd22a141f8e50ca0b6e7cf
Hou Tianze <houtianze@users.noreply.github.com>
2019-05-20 14:54:58 +0800

    Add an abortion banner for release.sh when interrupted

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 #set -o errexit
 #set -x
 
-trap "exit" SIGINT
+trap "echo '=== Release script interrupted ==='; exit -1" SIGINT
 
 py2venv=${py2venv-"$HOME/Documents/t/pyvenv/2.7.16"}
 py3venv=${py3venv-"$HOME/Documents/t/pyvenv/3.7.3"}
